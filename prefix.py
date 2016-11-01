48dcce31cc20a571b7d4108785682c6aa3662c49
Andrew Phillips <skeledrew@gmail.com>
2016-11-01 14:27:05 -0500

    Prefix app [Token not found error]

--- prefix.py
+++ prefix.py
@@ -10,5 +10,7 @@ lst = []
 for string in dic['array']:
     if not string[:len(dic['prefix'])] == dic['prefix']:
         lst.append(string)
-print({'token': verify['token'], 'array': lst})
-print('Result: ' + requests.post('http://challenge.code2040.org/api/prefix/validate', data={'token': verify['token'], 'array': lst}).text)
+
+result = json.dumps({'token': verify['token'], 'array': lst})
+print(result)
+print('Result: ' + requests.post('http://challenge.code2040.org/api/prefix/validate', data=result).text)
