f57b89459521612ac467f19ae147ff38f1e17705
Andrew Phillips <skeledrew@gmail.com>
2016-11-02 13:33:45 -0500

    Intermediate version

--- prefix.py
+++ prefix.py
@@ -10,5 +10,7 @@ lst = []
 for string in dic['array']:
     if not string[:len(dic['prefix'])] == dic['prefix']:
         lst.append(string)
-print({'token': verify['token'], 'array': lst})
-print('Result: ' + requests.post('http://challenge.code2040.org/api/prefix/validate', data={'token': verify['token'], 'array': lst}).text)
+
+result = json.dumps({'token': verify['token']})
+print(result)
+print('Result: ' + requests.post('http://challenge.code2040.org/api/prefix/validate', data=result, headers={'content-type': 'application/json'}).text)
